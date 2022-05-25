8650f307774359a4c8de964634c9fe8df8e77a41
sojwalwherrelz <104896718+sojwalwherrelz@users.noreply.github.com>
2022-05-25 13:53:37 +0530

    Update PlayersInfo.vue

--- PlayersInfo.vue
+++ PlayersInfo.vue
@@ -1,15 +1,38 @@
 <template >
 <div>
-<h1>Players List Data</h1>
+<h1>Players Info Data</h1>
     <table border='1px'>
         <tr>
             <td>Name</td>
             <td>Counrty Name</td>
+            <td>Role</td>
+            <td>Batting Style</td>
+            <td>Place Of Birth</td>
+            <td>Stats</td>
         </tr>
-        <tr v-for=" item in list " v-bind:key="item.id">
-            <td>{{item.name}}</td>
-            <td>{{item.country}}</td>              
-            </tr>
+        <tr>
+            <td>{{name}}</td>
+            <td>{{country}}</td>              
+            <td>{{role}}</td>              
+            <td>{{battingStyle}}</td>              
+            <td>{{placeOfBirth}}</td>              
+            <td>
+                <ul v-for="stat in stats" v-bind:key="stat.id">
+                        <li>
+                            fn : {{stat.fn}}
+                        </li>
+                        <li>
+                            matchtype : {{stat.matchtype}}
+                        </li>
+                        <li>
+                            stat :{{stat.stat}}
+                        </li>
+                        <li>
+                            value : {{stat.value}}
+                        </li>
+                </ul>
+            </td>              
+        </tr>
     </table>
 </div>
 </template>
@@ -18,22 +41,34 @@
 import axios from "axios"
 
 export default {
-       apikey:'%APIKEY%',
-        name:'PlayersApi',
+    
+        name:'PlayerInfo',
         data(){
             return {
-                list:undefined
+                list:undefined,
+                dateOfBirth:undefined,
+                name:undefined,
+                role:undefined,
+                battingStyle:undefined,
+                placeOfBirth:undefined,
+                country:undefined,
+                stats:undefined,
             }
         },
 
         mounted(){
-        const apikey = '%APIKEY%';
-            const Name = "Olive Smith"
-            const playerName = Name.replace(" ", "%20");
-            axios.get(`https://api.cricapi.com/v1/players?apikey=${apikey}&offset=0&search=${playerName}`)
+            const apikey = '%APIKEY%';
+            const id = 'e9f47702-80c0-4a8f-91ff-61d391624ae6';
+            axios.get(`https://api.cricapi.com/v1/players_info?apikey=${apikey}&id=${id}`)
             .then(res => {
                 console.warn(res.data.data);
-                this.list  = res.data.data;
+                this.name  = res.data.data.name;
+                this.dateOfBirth  = res.data.data.dateOfBirth;
+                this.role  = res.data.data.role;
+                this.battingStyle  = res.data.data.battingStyle;
+                this.placeOfBirth  = res.data.data.placeOfBirth;
+                this.country  = res.data.data.country;
+                this.stats  = res.data.data.stats;
             }
             )
         }
