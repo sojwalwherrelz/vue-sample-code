e134163d9c91f66cbf1c8c3ff3ffad062aadd359
sojwalwherrelz <104896718+sojwalwherrelz@users.noreply.github.com>
2022-05-25 17:44:01 +0530

    Update MatchInfo.vue

--- MatchInfo.vue
+++ MatchInfo.vue
@@ -8,8 +8,7 @@
             <td>Date time GMT</td>
             <td>match type</td>
             <td>Status</td>
-            <td>Venue</td>
-           
+            <td>Venue</td>           
         </tr>
         <tr >
             <td>{{name}}</td>
@@ -23,11 +22,8 @@
 </div>
 </template>
 <script>
-
 import axios from "axios"
-
 export default {
-        
         name:'SeriesInfo',
         data(){
             return {
@@ -39,23 +35,18 @@ export default {
                 statsu:undefined,
             }
         },
-
         mounted(){
             const apikey = '%APIKEY%';
             axios.get(`https://api.cricapi.com/v1/match_info?apikey=${apikey}&id=7d4496c7-f353-4ad9-b20f-a6314e181acf`)
             .then(res => {
-                console.log(res.data.data.name)
-                this.name  = res.data.data.name;
-                this.date  = res.data.data.date;
-                this.dateTimeGMT  = res.data.data.dateTimeGMT;
-                this.matchType  = res.data.data.matchType;
-                this.venue  = res.data.data.venue;
-                this.status  = res.data.data.status;
-               
-                
+                this.name           = res.data.data.name;
+                this.date           = res.data.data.date;
+                this.dateTimeGMT    = res.data.data.dateTimeGMT;
+                this.matchType      = res.data.data.matchType;
+                this.venue          = res.data.data.venue;
+                this.status         = res.data.data.status;       
             }
             )
         }
-    
 }
 </script>
