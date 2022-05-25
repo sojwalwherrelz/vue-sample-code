8a9644931b200ac0b92d845d8b7c623cc63e0abf
sojwalwherrelz <104896718+sojwalwherrelz@users.noreply.github.com>
2022-05-25 17:43:33 +0530

    Update FantacySquad.vue
    update code format

--- FantacySquad.vue
+++ FantacySquad.vue
@@ -1,6 +1,6 @@
 <template >
 <div>
-<h1>Fantacy Squad Info</h1>
+    <h1>Fantacy Squad Info</h1>
     <table border="1px">
         <tr>
             <td>Name</td>
@@ -25,37 +25,28 @@
                     <li>
                         Country : {{ teamName.country}}
                     </li>
-                    
-
                 </ul>
             </td>
         </tr>
-
     </table>
 </div>
 </template>
 <script>
-
 import axios from "axios"
-
 export default {
-    
         name:'FantacySquad',
         data(){
             return {
                 list:undefined
             }
         },
-
         mounted(){
-            const apikey = 'da2589ff-76cf-4b11-8654-080868ac896b';
+            const apikey = '%APIKEY%';
             axios.get(`https://api.cricapi.com/v1/match_squad?apikey=${apikey}&id=c50e6130-6718-4c11-b072-df382d24ed1c`)
             .then(res => {
-                console.warn(res.data.data);
                 this.list  = res.data.data;
             }
             )
-        }
-    
+        }    
 }
 </script>
